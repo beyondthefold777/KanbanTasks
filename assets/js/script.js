21ded3e181a739c9db66381a8f0f4ed83480dfaf
beyondthefold777 <beyondthefold1989@gmail.com>
2024-08-11 00:17:43 -0400

    this function updates the nextid variable in local storage each time a new idea is generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() function generateTaskId() {
+ function generateTaskId() {
     let currentId = nextId;
     nextId++;
     localStorage.setItem("nextId", nextId);
@@ -15,9 +15,17 @@ function generateTaskId() function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
-}
-
+    // here we have our dynamically created task card
+    let taskCard = `<div class="card task-card mb-3" id="task-${task.id}">
+                      <div class="card-body">
+                        <h5 class="card-title">${task.title}</h5>
+                        <p class="card-text">${task.description}</p>
+                        <p class="card-text">Deadline: ${task.deadline}</p>
+                        <button class="btn btn-danger delete-btn">Delete</button>
+                      </div>
+                    </div>`;
+    return taskCard;
+  }
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
